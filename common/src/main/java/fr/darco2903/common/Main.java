f9b509d32c6a46f7ed796ba43f39617fe5168df8
Darco2903 <114527594+Darco2903@users.noreply.github.com>
2025-07-27 15:18:44 +0200

    project build

--- common/src/main/java/fr/darco2903/common/Main.java
+++ common/src/main/java/fr/darco2903/common/Main.java
@@ -1,10 +1,9 @@
 package fr.darco2903.common;
 
-import com.sun.istack.internal.NotNull;
 import fr.darco2903.logger.LoggerInterface;
 
 public class Main {
-    public static void sendNotify(@NotNull LoggerInterface logger) {
+    public static void sendNotify(LoggerInterface logger) {
         SdDaemon sdDaemon = new SdDaemon(logger);
 
         if (sdDaemon.booted()) {
